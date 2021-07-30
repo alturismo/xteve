b0b2276a638d8a317f4666d32f48b8d9c50e9ae0
alturismo <alturismo@gmail.com>
2021-07-30 11:07:14 +0200

    update cronjob.sh
    added reverse proxy rewrite to make cached images and icons working for https://xteve.yourdomain.de/

--- cronjob.sh
+++ cronjob.sh
@@ -3,13 +3,25 @@
 ##### Config
 
 use_xTeveAPI="no"
+use_xTeveRP="no"
 use_TVH_Play="no"
 use_TVH_xml="no"
 use_TVH_move="no"
 
+
 ### xTeve ip, Port in case API is used to update XEPG
 xTeveIP="192.168.1.2"
 xTevePORT="34400"
+
+### setup rewrite rule für Reverse Proxy https xml usage
+# the rewritten url will be then http://yourxtevedomain.de/xmltv/xteverp.xml
+xtevelocal="http://192.168.1.2:34400"
+xteveRP="https://xteve.mydomain.de"
+xtevelocalfile="/root/.xteve/data/xteve.xml"
+xteveRPfile="/root/.xteve/data/xteverp.xml"
+# if wished, a ready RP m3u file generator, url will be http://yourxtevedomain.de/xmltv/xteverp.m3u
+xtevelocalm3ufile="/root/.xteve/data/xteve.m3u"
+xteveRPm3ufile="/root/.xteve/data/xteverp.m3u"
 
 ### TVHeadend ip, Port in case m3u Playlist is wanted
 TVHIP="192.168.1.2"
@@ -69,6 +81,25 @@ if [ "$use_xTeveAPI" = "yes" ]; then
 		sleep 1
 		curl -X POST -d '{"cmd":"update.xepg"}' http://$xTeveIP:$xTevePORT/api/
 		sleep 1
+	fi
+fi
+
+# create rewritten xml and m3u file
+if [ "$use_xTeveRP" = "yes" ]; then
+	if [ -z "$xteveRPfile" ]; then
+		echo "no Path credential"
+	else
+		sleep 10
+		cp $xtevelocalfile $xteveRPfile
+		sed -i "s;$xtevelocal;$xteveRP;g" $xteveRPfile
+		sleep 2
+		gzip -kf $xteveRPfile
+	fi
+	if [ -z "$xteveRPm3ufile" ]; then
+		echo "no Path credential"
+	else
+		cp $xtevelocalm3ufile $xteveRPm3ufile
+		sed -i "s;$xtevelocal;$xteveRP;g" $xteveRPm3ufile
 	fi
 fi
 
